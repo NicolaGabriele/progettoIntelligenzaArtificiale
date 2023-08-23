b981e41dd64000df4f30e9cc4ad9378936321d90
Nicola <nicolagabriele99@gmail.com>
2023-08-23 10:40:03 +0200

    euristica basata sul numero di atomi del goal ancora da soddisfare

--- punto 2/EmergencyProblemHeuristic.java
+++ punto 2/EmergencyProblemHeuristic.java
@@ -1,19 +1,10 @@
 package fr.uga.pddl4j.heuristics.state;
 
-import fr.uga.pddl4j.heuristics.state.RelaxedGraphHeuristic;
-import fr.uga.pddl4j.parser.DefaultParsedProblem;
-import fr.uga.pddl4j.parser.Parser;
-import fr.uga.pddl4j.planners.InvalidConfigurationException;
-import fr.uga.pddl4j.planners.LogLevel;
-import fr.uga.pddl4j.planners.ProblemNotSupportedException;
-import fr.uga.pddl4j.planners.statespace.FF;
 import fr.uga.pddl4j.planners.statespace.search.Node;
 import fr.uga.pddl4j.problem.*;
-import fr.uga.pddl4j.problem.operator.Action;
 import fr.uga.pddl4j.problem.operator.Condition;
+import fr.uga.pddl4j.util.BitVector;
 
-import java.io.FileNotFoundException;
-import java.util.Arrays;
 
 public class EmergencyProblemHeuristic extends RelaxedGraphHeuristic {
 
@@ -27,24 +18,26 @@ public class EmergencyProblemHeuristic extends RelaxedGraphHeuristic {
     @Override
     public int estimate(State state, Condition goal) {
         super.setGoal(goal);
-        final int level = this.expandRelaxedPlanningGraph(state);
-        final int max = super.getMaxValue();
-        final double value =(level > 3)?max:super.getSumValue();//super.getSumValue()+alpha*(level-super.getMaxValue());
-        alpha-=rate;
-        //System.out.println(value);
-        return (isGoalReachable())?(int)value:Integer.MAX_VALUE;
-
+        this.expandRelaxedPlanningGraph(state);
+        BitVector positiveFluents = goal.getPositiveFluents();
+        int count = 0;
+        for(int i = positiveFluents.nextSetBit(0); i>=0; i = positiveFluents.nextSetBit(i+1)){
+            BitVector tmp = new BitVector();
+            tmp.set(i);
+            if(state.satisfy(new Condition(tmp,new BitVector())))count++;
+        }
+        BitVector negativeFluents = goal.getNegativeFluents();
+        for(int i = negativeFluents.nextSetBit(0); i>=0; i = negativeFluents.nextSetBit(i+1)){
+            BitVector tmp = new BitVector();
+            tmp.set(i);
+            if(state.satisfy(new Condition(tmp,new BitVector())))count++;
+        }
+        int value = goal.cardinality() - count;
+        return value;
     }
 
     @Override
     public double estimate(Node node, Condition goal) {
         return this.estimate((State)node,goal);
-        /*super.setGoal(goal);
-        final int level = this.expandRelaxedPlanningGraph(node);
-        System.out.println(level+"\n"+"MAX_VALUE: "+super.getMaxValue()+"\n");
-        final double value =super.getSumValue()+alpha*(level-super.getMaxValue());
-        alpha-=rate;
-        //System.out.println(value);
-        return (isGoalReachable())?value:Integer.MAX_VALUE;*/
     }
 }
